1aafae18848b7c93555f84a1da9c01da85ebac05
jafarchik <jafarovja@yandex.ru>
2025-09-24 02:14:27 +0700

    creating nodes almost done

--- Assets/App/States/Constructor/CreateNodesState.cs
+++ Assets/App/States/Constructor/CreateNodesState.cs
@@ -1,5 +1,6 @@
 using Shafir.FSM;
 using Shafir.UI;
+using UnityEngine;
 
 namespace Shafir.App
 {
@@ -11,20 +12,55 @@ namespace Shafir.App
 
         private const string LegendText = "Добавление вершин";
 
+        private Plane _raycastPlane;
+
+        private ulong _maxNodeId;
+
         public CreateNodesState(AppContext appContext)
         {
             _appContext = appContext;
+
+            _raycastPlane = new Plane(Vector3.zero, Vector3.up, Vector3.right);
         }
 
         public void Enter()
         {
+            _appContext.UserInput.LeftMouseButtonClicked += OnLeftMouseButtonClicked;
+
             _appContext.LegendWindow.Show();
             _appContext.LegendWindow.SetLegend(LegendText);
+
+            _maxNodeId = ulong.MinValue;
+
+            var nodes = _appContext.GraphView.Model.Nodes;
+            foreach (var node in nodes.Values)
+            {
+                if (node.Id > _maxNodeId)
+                    _maxNodeId = node.Id;
+            }
+
+            _maxNodeId++;
         }
 
         public void Exit()
         {
+            _appContext.UserInput.LeftMouseButtonClicked -= OnLeftMouseButtonClicked;
+
             _appContext.LegendWindow.Hide();
+        }
+
+        private void OnLeftMouseButtonClicked()
+        {
+            var camera = _appContext.MainCamera.Camera;
+            var ray = camera.ScreenPointToRay(_appContext.UserInput.MousePosition);
+
+            if (_raycastPlane.Raycast(ray, out var distance) == false)
+                return;
+
+            var clickPos = ray.GetPoint(distance);
+
+            _appContext.GraphView.Model.AddNode(_maxNodeId, clickPos);
+            _maxNodeId++;
         }
     }
 }
--- Assets/GraphVIew/GraphModel.cs
+++ Assets/GraphVIew/GraphModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,11 +13,35 @@ namespace Shafir.GraphViews
     /// </remarks>
     public class GraphModel
     {
+        public event Action<ulong> NodeAdded;
+        public event Action<ulong> NodeRemoved;
+
+        public event Action<ulong> EdgeAdded;
+        public event Action<ulong> EdgeRemoved;
+
         public IReadOnlyDictionary<ulong, NodeModel> Nodes => _nodes;
         public IReadOnlyDictionary<ulong, EdgeModel> Edges => _edges;
 
         private Dictionary<ulong, NodeModel> _nodes = new();
         private Dictionary<ulong, EdgeModel> _edges = new();
+
+        public void AddNode(ulong nodeId, Vector3 nodePosition)
+        {
+            var nodeModel = new NodeModel(nodeId, nodePosition);
+            _nodes.Add(nodeId, nodeModel);
+
+            NodeAdded?.Invoke(nodeId);
+        }
+
+        public void RemoveNode(ulong nodeId)
+        {
+            if (_nodes.TryGetValue(nodeId, out var node) == false)
+                return;
+
+            node.ClearEdges();
+
+            NodeRemoved?.Invoke(nodeId);
+        }
 
         public void AddEdge
         (
@@ -31,6 +56,8 @@ namespace Shafir.GraphViews
             startNode.AddEdge(newEdge);
             endNode.AddEdge(newEdge);
             _edges.Add(edgeId, newEdge);
+
+            EdgeAdded?.Invoke(edgeId);
         }
 
         public void RemoveEdge(ulong edgeId)
@@ -39,6 +66,8 @@ namespace Shafir.GraphViews
             RemoveEdgeFromNode(edge, edge.StartNode);
             RemoveEdgeFromNode(edge, edge.EndNode);
             _edges.Remove(edgeId);
+
+            EdgeRemoved?.Invoke(edgeId);
         }
 
         private NodeModel CreateOrPickNode(ulong id, Vector3 pos)
